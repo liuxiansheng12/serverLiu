89df14136a0b037f2529205ea044212ea70be244
liushanzhuang <liushanzhuang@metrodata.cn>
2022-01-07 17:35:08 +0800

    20220107

--- bs.sh
+++ bs.sh
@@ -2,12 +2,10 @@ fileName=public
 
 zip -q -r ${fileName}.zip ${fileName}
 
-echo 密码：1996L@lsz.0
 scp ./${fileName}.zip root@116.62.144.18:~/node_demo/serverLiu 
 
 rm -rf ${fileName}.zip
 
-echo 密码：1996L@lsz.0
 ssh -tt root@116.62.144.18<<EOF
 cd node_demo
 cd serverLiu
